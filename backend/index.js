fe5b296a20ae2b79c57aa27d5aa2b80ec859fb30
maurimdn <neipanmauricio@gmail.com>
2022-05-21 14:34:59 -0300

    agregue database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 
 const express = require('express')
 var cors = require('cors')
+const { Client } = require('pg')
+
 
 
 const app = express()
@@ -16,7 +18,8 @@ app.get('/data', (req, res) => {
 })
 
 app.post('/data', (req, res) => {
-    console.log(req.body)
+    const userData = req.body
+    saveUserDate(userData.nombre, userData.password)
   })
 
 
@@ -25,3 +28,30 @@ app.post('/data', (req, res) => {
 app.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`)
 })
+
+async function saveUserDate(name, password){
+
+const client = new Client({
+  host: 'ec2-3-229-11-55.compute-1.amazonaws.com',
+  user: 'ykapccheefqopy',
+  password: '14717759fbaf2535c674de8df4ffe62bcc85a5387602052c95ba23d3a3998d6c',
+  database: 'd5b20rocsjrval',
+  port:5432,
+  ssl:{
+      rejectUnauthorized: false,
+  },
+});
+
+
+
+await client.connect();
+
+const queryToInsert = "INSERT INTO registro_pe (nombre, contrasena)VALUES ('"+name+"','"+password+"')"
+
+const res = await client.query(queryToInsert)
+const resultado = res.rows;
+await client.end()
+return(resultado)
+
+}
+
